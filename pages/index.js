5851e61946ca68608ed496ec6d025c3c94bce230
Luis <luisglopez7777@gmail.com>
2021-01-06 13:41:33 -0600

    No mailgun

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ const HomePage = () => {
             setState("SUCCESS");
         } catch (e) {
             setState("ERROR");
-            console.log(e)
+            console.log('error', e)
             setErrorMessage('Uuups... Algo salió mal. Envíanos un correo a matchmakingtech@gmail.com y nos pondremos en contacto')
         }
     }
